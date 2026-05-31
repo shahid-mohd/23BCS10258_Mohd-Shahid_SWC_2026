6e1dfa136705663ca8954ad51dcad84690bfdc91
Mohd Shahid <155215858+shahid-mohd@users.noreply.github.com>
2026-05-31 23:03:22 +0530

    Add SQL query for acceptance rate by date

---
+++ Session_01/Acceptance_Rate_By_Date.sql
@@ -0,0 +1,13 @@
+SELECT s.date,
+    ROUND(
+        COUNT(a.user_id_sender)::NUMERIC / COUNT(*)::NUMERIC, 2
+    ) AS acceptance_rate
+FROM fb_friend_requests s
+LEFT JOIN fb_friend_requests a
+    ON s.user_id_sender = a.user_id_sender
+   AND s.user_id_receiver = a.user_id_receiver
+   AND a.action = 'accepted'
+WHERE s.action = 'sent'
+GROUP BY s.date
+HAVING COUNT(a.user_id_sender) > 0
+ORDER BY s.date;
